3baa1080c8dc2d03b737de31bdcea48da731c517
RopVictor <victorrop99@gmail.com>
2023-03-17 17:03:41 -0700

    putchar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 int i, j, k;
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 10; i++)
 
 {
 
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 99; i++)
+{
+for (j = i; j <= 99; j++)
+
+{
+
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+
+/* print second two-digit number */
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+
+/* print comma and space unless it's the last combination */
+if (i != 99 || j != 99)
+{
+putchar(',');
+putchar(' ');            }
+}
+}
+
+putchar('\n');
+
+return (0);
+}
