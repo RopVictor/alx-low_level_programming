e28469cde8cb0751ac4caa66ba10730462207119
RopVictor <victorrop99@gmail.com>
2023-03-18 14:31:12 -0700

    No errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,20 @@
 #include <unistd.h>
 
-int main() {
-  char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, message, 50); // 50 is the length of the message including the newline character
-  return 1;
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = 0;
+
+while (msg[len])
+len++;
+
+if (write(STDERR_FILENO, msg, len) != len)
+return (1);
+
+return (1);
 }
