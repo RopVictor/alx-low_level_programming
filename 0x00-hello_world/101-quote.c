608a0eaf07e24bdaebe033b97c20692f3afc069a
RopVictor <victorrop99@gmail.com>
2023-03-18 14:34:51 -0700

    No errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char *msg = "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n";
 ssize_t len = 0;
 
 while (msg[len])
