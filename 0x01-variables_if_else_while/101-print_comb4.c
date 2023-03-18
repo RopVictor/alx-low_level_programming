3f95fec94fde6b0e91289a896a296b460a73c8fd
RopVictor <victorrop99@gmail.com>
2023-03-17 17:16:07 -0700

    putchar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 int i, j, k;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 
 {
 
