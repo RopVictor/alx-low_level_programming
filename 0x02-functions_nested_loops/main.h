9967437c921c8ce9289bbd62f1c25fad6913f141
RopVictor <victorrop99@gmail.com>
2023-03-23 08:04:06 -0700

    more of functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototypes */
+
 int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
 
 #endif /* MAIN_H */
