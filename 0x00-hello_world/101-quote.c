84ac2d2f4dcc1710826df2a8a7554a264608450a
RopVictor <victorrop99@gmail.com>
2023-05-18 16:21:28 -0700

    unnixl

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 const char *msg =
-"and that piece of art is useful\" -Dora Korpar, 2015-10-19";
+"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
 ssize_t len = 0;
 
 while (msg[len])
