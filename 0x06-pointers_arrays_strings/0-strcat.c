42ab7a4bf1188f33af6f7098ee16bdd1412525a1
RopVictor <victorrop99@gmail.com>
2023-05-31 12:53:53 -0700

    conc

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the resulting string @dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	/* Find the length of the destination string */
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	/* Concatenate the source string to the destination string */
+	while (src[i] != '\0') {
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+
+	dest[dest_len] = '\0'; /* Add a terminating null byte */
+
+	return dest;
+}
