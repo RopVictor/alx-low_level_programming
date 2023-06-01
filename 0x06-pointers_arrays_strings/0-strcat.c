a595c014cc19307da5703dcc98933a416a4ac1aa
RopVictor <victorrop99@gmail.com>
2023-06-01 14:02:34 -0700

    adv

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,27 +3,27 @@
  * @dest: The destination string.
  * @src: The source string.
  *
- * Return: A pointer to the resulting string @dest.
+ * This function appends the src string to the dest string, overwriting the
+ * terminating null byte ('\0') at the end of dest, and then adds a new
+ * terminating null byte. The strings are concatenated by copying the
+ * characters from the source string to the end of the destination string.
+ *
+ * Return: A pointer to the resulting string dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = 0;
-	int i = 0;
+	int i, j;
 
-	/* Find the length of the destination string */
-	while (dest[dest_len] != '\0')
-		dest_len++;
+	/* Find the end of the destination string */
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-	/* Concatenate the source string to the destination string */
-while (src[i] != '\0') 
-	{
-		dest[dest_len] = src[i];
-		dest_len++;
-		i++;
-	}
+	/* Append the source string to the destination string */
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
 
-dest[dest_len] = '\0'; /* Add a terminating null byte */
+	/* Add the terminating null byte */
+	dest[i + j] = '\0';
 
-return (dest);
-
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,11 @@ char *_strncpy(char *dest, const char *src, int n)
 {
 	int i;
 
-	/* Copy at most n characters from the source string to the destination string */
+	/* Copy at most n characters from the source*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	/* If n is greater than the length of src, pad the remaining characters with null */
+	/* If n is greater than the length*/
 	for (; i < n; i++)
 		dest[i] = '\0';
 
